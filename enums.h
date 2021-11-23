9db8ada25fe47c1403aafb17f983a0dc42d17c5e
Milacic, E <e.milacic@tue.nl>
2021-11-23 09:12:19 +0100

    Added an enum.h file to store lots of enums.

---
+++ enums.h
@@ -0,0 +1,12 @@
+/* enums - Contains the enums used in this project
+ * E.Milacic
+ */
+
+#ifndef LCDButtons_h
+#define LCDButtons_h
+
+#include "Arduino.h"
+
+enum class KeypadButtons{UP; DOWN; RIGHT; LEFT; SELECT; NONE};
+
+#endif
